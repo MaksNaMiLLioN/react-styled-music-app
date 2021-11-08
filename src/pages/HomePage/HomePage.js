ab2ff321ada503924ba30c6a9edc8fea70eba7b3
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-11-08 15:35:21 +0600

    Complete Content

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
+import { heroOne, heroTwo, heroThree, pricingData } from '../../data/homeData';
+import Content from '../../components/Content/Content'
 
 const HomePage = () => {
     return (
         <>
         <Hero />
+        <Content {...heroOne}/>
+        <Content {...heroTwo}/>
+        <Content {...heroThree}/>
         </>
     )
 }

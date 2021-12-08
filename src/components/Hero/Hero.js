cdd3a04ff95e71b4653c7aa820545e4aa3545f82
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-12-08 10:24:30 +0600

    Complete Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { Button, Container, Column, MainHeading, Row, TextWrapper } from '../../globalStyles';
-import {BiPlayCircle} from 'react-icons/bi';
-import {HeroRow, HeroSection, Heading, HeroText, HeroColumn, ButtonContainer} from './HeroStyles'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Column,
+  MainHeading,
+  Row,
+  TextWrapper,
+} from "../../globalStyles";
+import { BiPlayCircle } from "react-icons/bi";
+import { HeroSection, ButtonContainer } from "./HeroStyles";
 
 const Hero = () => {
-    return (
-        <HeroSection id ="hero">
-        <Row justify='center' align='center' height='100%' padding='2rem'>
-            <Column align="center" mb="10%">
-                <MainHeading>Every Creative Music</MainHeading>
-                <TextWrapper
-                color="white"
-                size='clamp(1rem,2vw,1.3rem)'
-                lineHeight='1.1rem'
-                maxWidth='480px'
-                align="center"
-                >
-                    Discover, stream and share a constantly expending mix of music from emerging and major
-                    artists around the world
-                </TextWrapper>
-                <ButtonContainer>
-                    <Link to="sign-up">
-                        <Button big fontBig>
-                            Get Started
-                        </Button>
-                    </Link>
-                    <Button big fontBig>
-                        <BiPlayCircle size='2.3rem'></BiPlayCircle>
-                    </Button>
-                </ButtonContainer>
-            </Column>
-        </Row>    
-        </HeroSection>
-    )
-}
+  return (
+    <HeroSection id="hero">
+      <Row justify="center" align="center" height="100%" padding="2rem">
+        <Column align="center" mb="10%">
+          <MainHeading>Every Creative Music</MainHeading>
+          <TextWrapper
+            color="white"
+            size="clamp(1rem,2vw,1.3rem)"
+            lineHeight="1.1rem"
+            maxWidth="480px"
+            align="center"
+          >
+            Discover, stream and share a constantly expending mix of music from
+            emerging and major artists around the world
+          </TextWrapper>
+          <ButtonContainer>
+            <Link to="sign-up">
+              <Button big fontBig>
+                Get Started
+              </Button>
+            </Link>
+            <Button big fontBig>
+              <BiPlayCircle size="2.3rem"></BiPlayCircle>
+            </Button>
+          </ButtonContainer>
+        </Column>
+      </Row>
+    </HeroSection>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import { heroOne, heroTwo, heroThree, pricingData } from '../../data/homeData';
-import Content from '../../components/Content/Content'
-import Pricing from '../../components/Pricing/Pricing';
-import Footer from '../../components/Footer/Footer';
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import { heroOne, heroTwo, heroThree } from "../../data/homeData";
+import Content from "../../components/Content/Content";
+import Pricing from "../../components/Pricing/Pricing";
+import Footer from "../../components/Footer/Footer";
 
 const HomePage = () => {
-    return (
-        <>
-        <Hero />
-        <Content {...heroOne}/>
-        <Content {...heroTwo}/>
-        <Content {...heroThree}/>
-        <Pricing />
-        <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Hero />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
+      <Pricing />
+      <Footer />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
